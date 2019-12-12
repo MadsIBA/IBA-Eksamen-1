5b9d5bbb6e5e03249b46dbcf4a39db7515c1dae3
Mads <Mads2097@gmail.com>
2019-12-12 13:42:03 +0100

    Version 0.61 - Beta +Step1 +Step2 +PaymentOption
    ChangeLog: Version 0.61 - Beta +Step1 +Step2 +PaymentOption
    
    -Added Timeout Timer for Step1 & Step2 page
    
    ------------------------------------------------
    Version 0.601 - Beta +Step1 +Step2 +PaymentOption
    ------------------------------------------------
    -Added bg webM to start.html
    
    ------------------------------------------------
    Version 0.6 - Beta +Step1 +Step2 +PaymentOption
    ------------------------------------------------
    -Added PaymentOptions page
    -Added Payment Images
    -Added Payment Options Bar to start.html
    
    ------------------------------------------------
    Version 0.54 +Step1 +Step2
    ------------------------------------------------
    -"Pris" will now reset back to 0 if the user changes his/her mind on which cabin to use
    -Added empty page for payment options
    
    ------------------------------------------------
    Version 0.53 +Step1 +Step2
    ------------------------------------------------
    -Update to Cabin buttons will stay clicked when a time has been selected
    -Update to Fontsize of buttons that selects time
    -Update to HTML file - Changed code style
    
    ------------------------------------------------
    Version 0.52 +Step1 +Step2
    ------------------------------------------------
    -Update to Payment Button to be inactive at start.
    -Update to Payment Button border on active/inactive
    
    ------------------------------------------------
    Version: 0.51 Beta +Step1 +Step2:
    ------------------------------------------------
    Update to Payment Button to be inactive until user has picked a cabin and time.
    It also becomes inactive if user changes mind and picks a new Cabin.

--- js/step1.js
+++ js/step1.js
@@ -1,3 +1,31 @@
+attachEvent(window,'load',function(){
+    var idleSeconds = 10;
+    var idleTimer;
+    function resetTimer(){
+      clearTimeout(idleTimer);
+      idleTimer = setTimeout(whenUserIdle,idleSeconds*1000);
+    }
+    attachEvent(document.body,'mousemove',resetTimer);
+    attachEvent(document.body,'keydown',resetTimer);
+    attachEvent(document.body,'click',resetTimer);
+  
+    resetTimer(); // Start the timer when the page loads
+  });
+  
+  function whenUserIdle(){
+    window.location.href = "start.html";
+    }
+  
+  function attachEvent(obj,evt,fnc,useCapture){
+    if (obj.addEventListener){
+      obj.addEventListener(evt,fnc,!!useCapture);
+      return true;
+    } else if (obj.attachEvent){
+      return obj.attachEvent("on"+evt,fnc);
+    }
+  } 
+
+
 fetch('https://consolwebapi.pay4it.dk/api/Devices/Detail?deviceID=560', {
                     method: "GET",
                     headers: {
